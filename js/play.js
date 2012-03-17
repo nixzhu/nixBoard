df04800374f1e8fb9ac129f3ff49a0416ca6b2b9
nixzhu <zhuhongxu@gmail.com>
2012-03-17 18:04:20 +0800

    一部分落子逻辑，填充算法。现在可以处理本家落子后棋块无气的情况，但无气却可提吃对方的情况还未完成。

--- js/play.js
+++ js/play.js
@@ -1,7 +1,7 @@
 /* some global values */
 var pan = new Array(
-	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -16,6 +16,27 @@ var pan = new Array(
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+);
+var shadow = new Array(
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
@@ -38,6 +59,12 @@ function showPan() {
 				cxt.fillStyle="white";
 				cxt.fill();
 			}
+			else if (pan[i][j] === 7) { // fill color
+				cxt.beginPath();
+				cxt.arc((i+1)*30, (j+1)*30,15,0,2*Math.PI,false);
+				cxt.fillStyle="red";
+				cxt.fill();
+			}
 			else {
 				/*
 				cxt.beginPath();
@@ -57,7 +84,7 @@ function play(row, col) {
 	}
 	// 处理已有棋子在此
 	if (pan[row][col] != 0) {
-		alert("此处已有棋子！%d %d", row, col);
+		alert("此处已有棋子！");
 		return;
 	}
 
@@ -70,11 +97,82 @@ function play(row, col) {
 		落子，返回
 	 不可下（自己会死），返回
 */
-	have_air(row, col);
-	have_my_people(row, col);
+	var can_down = false; // 是否可落子
+	if (!have_air(row, col)) {
+		if (have_my_people(row, col)) {
+			make_shadow();
 
-	stone_down(row, col);
+			// 得到将落子的棋子的颜色
+			var color = 2; // 白
+			if (move_count % 2 === 0) { // 未落子前是白
+				color = 1; 
+			}
 
+			flood_fill(row, col, color);	
+			if (fill_block_have_air(row, col, color)) {
+				can_down = true;
+			} else {
+				
+			}
+			//shadow_to_pan();
+		} else {
+			// if can kill 
+				can_down = true;
+			// else
+		}
+	} else {
+		can_down = true;
+	}
+	if (can_down) {
+		stone_down(row, col);
+	}
+}
+/* 填充的区域周围是否有空 */
+function fill_block_have_air(row, col, color) {
+	for (var i = 0; i < pan.length; i++) {
+		for (var j = 0; j < pan[i].length; j++) {
+			if (i !== row && j !== col) {
+				if (shadow[i][j] === 7 && pan[i][j] !== color) {
+					return true; // 此块有空，可下
+				}
+			}
+		}
+	}
+	alert("fill block 无气！！！");
+	return false;
+}
+/* 将盘面做个影分身 */
+function make_shadow() {
+	for (var i = 0; i < pan.length; i++) {
+		for (var j = 0; j < pan[i].length; j++) {
+			shadow[i][j] = pan[i][j];
+		}
+	}
+}
+function shadow_to_pan() {
+	for (var i = 0; i < pan.length; i++) {
+		for (var j = 0; j < pan[i].length; j++) {
+			pan[i][j] = shadow[i][j];
+		}
+	}
+}
+
+/* 泛洪填充，只操作影分身 */
+function flood_fill(row, col, color) { // color 为当前将要落子的棋子色
+	if (row < 0 || row > 19-1 || col < 0 || col > 19-1)
+		return;
+
+	var anti_color = 2;
+	if (color === 2)
+		anti_color = 1;
+
+	if (shadow[row][col] !== anti_color && shadow[row][col] !== 7) { // 非对方颜色，己方或空
+		shadow[row][col] = 7; // 表示已被填充
+		flood_fill(row+1, col, color);
+		flood_fill(row-1, col, color);
+		flood_fill(row, col+1, color);
+		flood_fill(row, col-1, color);
+	}
 }
 
 /* 坐标周围4交叉点有气否？ */
@@ -84,7 +182,7 @@ function have_air(row, col) {
 				pan[row-1][col] !== 0 &&
 				pan[row][col+1] !== 0 &&
 				pan[row][col-1] !== 0 ) {
-			//alert("have no air");
+			alert("have no air");
 			return false;
 		} else {
 			//alert("have air");
